168c3d13f1d78cff5cf9cf7431389c3fcaa6e371
cccczl <cccczl@hotmail.com>
2015-11-15 02:53:08 +0800

    Update v8.h

--- include/v8.h
+++ include/v8.h
@@ -1064,7 +1064,12 @@ class String : public Primitive {
   enum WriteOptions {
     NO_OPTIONS = 0,
     HINT_MANY_WRITES_EXPECTED = 1,
-    NO_NULL_TERMINATION = 2
+    NO_NULL_TERMINATION = 2,
+    PRESERVE_ASCII_NULL = 4,
+    // Used by WriteUtf8 to replace orphan surrogate code units with the
+    // unicode replacement character. Needs to be set to guarantee valid UTF-8
+    // output.
+    REPLACE_INVALID_UTF8 = 8
   };
 
   // 16-bit character codes.
